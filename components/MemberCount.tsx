01f3e498473c73d213fb39a1f91c65fee8a152e9
shaukat456 <shaukatfullstack134@gmail.com>
2024-06-20 17:09:08 +0500

    vixo

--- components/MemberCount.tsx
+++
@@ -1,9 +0,0 @@
-import NumberTicker from "../components/ui/number-ticker";
-
-export function MemberCount() {
-  return (
-    <p className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white">
-      <NumberTicker value={230} />
-    </p>
-  );
-}
--- components/NsaParallax.tsx
+++
@@ -1,9 +0,0 @@
-import ParallaxText from "./ParallaxText";
-
-export default function NsaParallax() {
-  return (
-    <main className="h-[300vh] bg-gradient-to-b from-blue-900 to-black overflow-hidden">
-      <ParallaxText />
-    </main>
-  );
-}
